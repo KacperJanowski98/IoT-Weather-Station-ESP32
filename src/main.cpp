b1af07e97029870a9ad02003606b6f179ceb1bda
Kacper Janowski <kacper.janowski98@wp.pl>
2021-10-19 12:16:43 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+const int LED_B = 13;
+
+void setup() 
+{
+  pinMode (LED_B, OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(LED_B, HIGH);
+  delay(1000);
+  digitalWrite(LED_B, LOW);
+  delay(1000);
+}
