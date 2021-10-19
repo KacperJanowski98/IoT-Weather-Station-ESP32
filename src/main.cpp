69597001b2946dbb5092231d01d9caf787884448
Kacper Janowski <kacper.janowski98@wp.pl>
2021-10-19 13:00:49 +0200

    Developed function to wifi connect..

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,45 @@
 #include <DHT.h>
 #include <WiFi.h>
 
-const int LED_B = 13;
+#define WIFI_TIMEOUT_MS 20000
+#define WIFI_NETWORK "AndroidAP_note8"
+#define WIFI_PASSWORD "sensor123"
+
+void connectToWiFi()
+{
+  Serial.print("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+
+  unsigned long startAttemptTime = millis();
+
+  // Keep looping while we're not connected and haven't reached the timeout
+  while (WiFi.status() != WL_CONNECTED &&
+          millis() - startAttemptTime < WIFI_TIMEOUT_MS)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  // Make sure we're actually connected, otherwise go to deep sleep
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println(" Failed!");
+    ESP.restart();  // Restrart ESP
+  }
+  else
+  {
+    Serial.print(" Connected!");
+    Serial.println(WiFi.localIP());
+  }
+}
 
 void setup() 
 {
-  pinMode (LED_B, OUTPUT);
+  
 }
 
 void loop() 
 {
-  digitalWrite(LED_B, HIGH);
-  delay(1000);
-  digitalWrite(LED_B, LOW);
-  delay(1000);
+
 }
